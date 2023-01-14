9fbab612901da1a5b370b5fcd6ccae1265099c9d
Aimen-Allah <aimenftaiti@gmail.com>
2023-01-14 21:33:28 +0100

    correction on var to let

--- server.js
+++ server.js
@@ -23,12 +23,7 @@ db.sequelize.sync()
   .catch((err) => {
     console.log("Failed to sync db: " + err.message);
   });
-
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
+  
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
